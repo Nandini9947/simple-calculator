a6c96f9a85e6450f008ac63aa4f885fb471ae708
Nandini9947 <guptanandini363@gmail.com>
2026-08-30 12:39:26 +0530

    Add files via upload

---
+++ # SIMPLE CALCULTOR.py
@@ -0,0 +1,10 @@
+# SIMPLE CALCULTOR : 4 operations are performed on any two numbers
+num1 = float(input("enter the first number: "))
+num2 = float(input("enter the second number: "))
+print("the sum of the two numbers:" , num1 + num2)
+print("the subtraction of the two numbers:" ,num1 - num2)
+print("the multiplication of the two numbers:" ,num1 * num2)
+print("the division of the two numbers:" ,num1 / num2)
+  
+
+
